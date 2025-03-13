318aea3d4b15313cf16412b5c8acd6a7eec8646a
Sara <sarailic431@gmail.com>
2025-03-13 19:15:03 +0100

    making classes

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ const addProjectForm = document.querySelector(".add-project-form");
 const addToDoEl = document.querySelector(".add-to-do-btn");
 const addToDoForm = document.querySelector(".add-to-do-form");
 
+const projectsList = document.querySelector(".projects-list");
+
 let inputProject;
 let inputToDoName;
 let inputDate;
@@ -32,10 +34,25 @@ addToDoEl.addEventListener("click", function (e) {
   addToDoForm.classList.remove("hidden");
 });
 
+class ProjectManager {
+  constructor() {
+    this.projects = [];
+    this.activeProject = null;
+  }
+
+  addProjects(project) {
+    this.projects.push(project);
+  }
+}
+
 class Project {
   constructor(projectName) {
     this.projectName = projectName;
     this.toDos = [];
+  }
+
+  setActiveProject(project) {
+    this.activeProject = project;
   }
 }
 
@@ -48,8 +65,22 @@ class ToDo {
   }
 }
 
+const projectManager = new ProjectManager();
+
 addProjectForm.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  const project = new Project(inputProject);
+  projectManager.addProjects(project);
+  console.log(project);
+  console.log(projectManager.projects);
+
+  const projectItem = document.createElement("li");
+  projectItem.innerHTML = `${inputProject} <button class="delete-project-btn">X</button>`;
+  projectItem.className = "project-item";
+
+  projectsList.appendChild(projectItem);
+  inputProjectNameEl.value = "";
 });
 
 addToDoForm.addEventListener("submit", function (e) {
