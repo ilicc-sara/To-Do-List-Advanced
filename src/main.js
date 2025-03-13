2b34d879a79a33366afe9374780cbae465468ba3
Sara <sarailic431@gmail.com>
2025-03-13 20:36:31 +0100

    seting bare functions like make list elements and adding objects to array

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ const addToDoEl = document.querySelector(".add-to-do-btn");
 const addToDoForm = document.querySelector(".add-to-do-form");
 
 const projectsList = document.querySelector(".projects-list");
+const toDoList = document.querySelector(".to-do-list");
 
 let inputProject;
 let inputToDoName;
@@ -59,12 +60,21 @@ class Project {
     this.id = crypto.randomUUID();
     this.toDos = [];
   }
+
+  addToDo(toDo) {
+    this.toDos.push(toDo);
+  }
+
+  removeToDo(id) {
+    this.toDos = this.toDos.filter((item) => item.id !== id);
+  }
 }
 
 class ToDo {
   constructor(name, date) {
     this.name = name;
     this.date = date;
+    this.id = crypto.randomUUID();
     this.isDone = false;
     this.isEditing = false;
   }
@@ -104,4 +114,41 @@ projectsList.addEventListener("click", function (e) {
 
 addToDoForm.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  const toDo = new ToDo(inputToDoName, inputDate);
+  // projectManager.addProjects();
+  projectManager.activeProject.addToDo(toDo);
+
+  console.log("array projekata", projectManager.projects);
+  // console.log(projectManager.activeProject.toDos);
+
+  const toDoItem = document.createElement("li");
+  toDoItem.setAttribute("data-id", toDo.id);
+
+  toDoItem.innerHTML = `<p class="title-text"> Title: <span class="title"> ${inputToDoName} </span> </p>
+            <div class="to-do-info">
+              <p class="date-text"> Date: <span class="date"> ${inputDate} </span></p>
+              <div class="btn-cont"> <button class="edit-to-do-btn">edit</button> <button class="delete-to-do-btn">delete</button> </div>
+              <input class="check" type="checkbox" />
+            </div>`;
+
+  toDoItem.className = "to-do-item";
+
+  toDoList.appendChild(toDoItem);
+
+  inputToDoNameEl.value = "";
+  inputDateEl.value = "";
+});
+
+toDoList.addEventListener("click", function (e) {
+  // prettier-ignore
+  if (!e.target.classList.contains("delete-to-do-btn")) return;
+  const target = e.target.closest(".to-do-item");
+
+  if (e.target.classList.contains("delete-to-do-btn")) {
+    target.remove();
+    // projectManager.removeProject(target.dataset.id);
+    projectManager.activeProject.removeToDo(target.dataset.id);
+    console.log(projectManager.activeProject.toDos);
+  }
 });
